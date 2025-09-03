90abfbb59c377da2b652c546663b9d765d8163d4
joseph101kt <joseph101kt@gmail.com>
2025-09-03 12:43:37 +0530

    added suspens to registrations/page.tsx

--- app/registrations/page.tsx
+++ app/registrations/page.tsx
@@ -1,12 +1,15 @@
 // app/registrations/page.tsx
 
 import EventRegistrations from "@/components/EventRegistrations";
+import { Suspense } from "react";
 
 
 
 
 export default function RegistrationsPage() {
   return (
+    <Suspense>
     <EventRegistrations/>
+    </Suspense>
   )
 }
