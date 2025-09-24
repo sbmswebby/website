4149c5bd4d26692a67383323e87e115ba3de314a
joseph101kt <joseph101kt@gmail.com>
2025-09-24 19:17:08 +0530

    updated

--- components/Register.tsx
+++ components/Register.tsx
@@ -20,6 +20,7 @@ export default function RegisterPage() {
           onClose={() => setShowModal(false)}
           onSubmit={handleManualSubmit}
           isProcessing={isProcessing}
+          sessionId= {sessionId}
         />
       )}
     </div>
