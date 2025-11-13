3930e46c26f93272f9c40004258ad47fd261b71e
joseph101kt <joseph101kt@gmail.com>
2025-11-13 12:59:27 +0530

    No changes: commit before redoing the id card and cretificate generation

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function HomePage() {
       <Hero />
       <AboutUs />
       <Competitions />
-      <Seminars />
+      <Seminars /> 
       <Academy />
       <Events /> 
       <Awards />
