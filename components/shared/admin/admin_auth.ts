266e98710ec74b6d2f76ca31d9f5a729a88bd83f
sbmswebby <sbmswebby@gmail.com>
2026-01-27 12:45:50 +0530

    tring to fix the admin auth

--- components/shared/admin/admin_auth.ts
+++ components/shared/admin/admin_auth.ts
@@ -1,7 +1,7 @@
 "use server";
 
 export async function verifyAdminPassword(password: string) {
-  const correctPassword = process.env.ADMIN_PASSWORD;
+  const correctPassword = process.env.NEXT_STRICT_ADMIN_PASSWORD;
 
   // DEBUG LOGS (Check these in Netlify Function Logs)
   console.log("Is ADMIN_PASSWORD defined?", !!correctPassword);
